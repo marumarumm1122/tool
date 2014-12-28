3015a2fd8b25223ccfe9037ee0c6c4de23a704e1
堀江佑介 <horieyuusuke@horieyuusuke-no-MacBook-Pro.local>
2014-12-29 04:38:01 +0900

    unused file deleted.

--- tools/HeaderGenerator.cpp
+++
@@ -1,8 +0,0 @@
-#include "HeaderGenerator.h"
-#include <stdio.h>
-using namespace tool;
-//ヘッダを作成してバッファに入れる.
-void CHeaderGenerator::Generate(){
-    printf(__func__);
-    printf("not implemented yet.\n");
-}
